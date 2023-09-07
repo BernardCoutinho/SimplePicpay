c093bb7618a7ea65985fd9b9a60435970c62d110
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-09-07 14:19:44 -0300

    feat: create transaction entity

---
+++ src/main/java/com/simplepicpay/domain/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package com.simplepicpay.domain.transaction;
+
+import com.simplepicpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "transactions")
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+    private BigDecimal amount;
+
+    private LocalDateTime timeStamp;
+}
